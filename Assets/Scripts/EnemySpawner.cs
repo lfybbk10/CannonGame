ccf5efaad436d7e12b4076180d63ad4332aff1f9
lfybbk10 <63192458+lfybbk10@users.noreply.github.com>
2023-07-20 01:13:08 +0300

    implement base enemy spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,7 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private int _spawnIntervalSec;
+
+    private float _spawnTimer;
     
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    private void OnDisable()
+    {
+        _spawnTimer = 0;
+        StopAllCoroutines();
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.05f);
+            _spawnTimer += 0.05f;
+            if (_spawnTimer >= _spawnIntervalSec)
+            {
+                _spawnTimer = 0;
+                SpawnEnemy();
+            }
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        throw new NotImplementedException();
+    }
 }
+
